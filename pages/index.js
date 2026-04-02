e1f3291fe8ef8fb88b5c4eb6f16de04dcb71db6a
Maike Silva <133704226+Senhoreme@users.noreply.github.com>
2026-04-02 00:48:52 +0000

    mais uma atualização

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home (){
-    return <h1>Se você tá lendo isso, me faça um pix "ThekillerGamer@outlook.com.br</h1>
+    return <h1>Se você tá lendo isso, me faça um pix "ThekillerGamer@outlook.com.br! 
+        New updates.
+    </h1>
 
 }
 export default Home; 
